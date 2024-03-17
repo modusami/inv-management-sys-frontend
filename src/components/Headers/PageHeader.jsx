99448173e1fe01f56b54bdf9bd27af78b7b9315e
modusami <128193218+modusami@users.noreply.github.com>
2024-03-17 16:23:21 -0400

    Update PageHeader.jsx

--- src/components/Headers/PageHeader.jsx
+++ src/components/Headers/PageHeader.jsx
@@ -1,8 +1,9 @@
 const PageHeader = () => {
 	return (
-		<div className="z-10 absolute top-5 left-1/2 transform -translate-x-1/2 w-full flex justify-center align-middle">
-			<p className="text-transparent bg-gradient-to-r from-pink-500 to-yellow-500 bg-clip-text font-semibold uppercase text-sm sm:text-sm md:text-l lg:text-xl xl:text-xl">
-				Inventory Management System By: <span className="text-black">Odusami</span>
+		<div className="w-full flex items-center justify-center">
+			<p className="text-transparent bg-gradient-to-r from-pink-500 lowercase to-yellow-500 bg-clip-text font-semibold text-sm sm:text-sm md:text-l lg:text-xl xl:text-xl">
+				Inventory Management System By:{" "}
+				<span className="text-black lowercase">Odusami</span>
 			</p>
 		</div>
 	);
