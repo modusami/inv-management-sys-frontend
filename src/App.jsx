4936407ba6e684caf139c18c8d2a117de164a09f
modusami <128193218+modusami@users.noreply.github.com>
2024-03-17 03:22:31 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,28 @@ import Canvas from "./components/Structures/Canvas";
 import "./App.css";
 import Button from "./components/Inventory/buttons/Button";
 import ReadScreen from "./components/Inventory/display/ReadScreen/ReadScreen";
+import CreateScreen from "./components/Inventory/display/CreateScreen/CreateScreen";
 
 function App() {
-	const [contentType, setContentType] = useState("home");
+	const [contentType, setContentType] = useState("create");
+
+	const handleScreen = (event) => {
+		const screenDisplayed = event.target.innerText.toLowerCase();
+		setContentType(screenDisplayed);
+	};
 
 	return (
 		<>
 			<Canvas>
 				<div className="flex w-full gap-5 justify-center my-3">
-					<Button text={"Create"} />
-					<Button text={"Read"} />
-					<Button text={"Update"} />
-					<Button text={"Delete"} />
+					<Button text={"Create"} onClick={handleScreen} />
+					<Button text={"Read"} onClick={handleScreen} />
+					<Button text={"Update"} onClick={handleScreen} />
+					<Button text={"Delete"} onClick={handleScreen} />
 				</div>
 				<div className="mt-6 w-full h-5"></div>
-				<ReadScreen />
+				{contentType == "create" && <CreateScreen />}
+				{contentType == "read" && <ReadScreen />}
 			</Canvas>
 		</>
 	);
