5750be604db425691def65d639aaac7698f51b96
modusami <128193218+modusami@users.noreply.github.com>
2024-03-17 02:51:16 -0400

    Update App & Canvas

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Canvas from "./components/Structures/Canvas";
 import "./App.css";
 
 function App() {
@@ -6,7 +7,19 @@ function App() {
 
 	return (
 		<>
-			<h1 className="text-3xl font-bold underline text-center">Hello world!</h1>
+			<Canvas>
+				<h1>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto placeat neque
+					eligendi deleniti ullam quasi iure nisi provident necessitatibus? Magnam quasi,
+					quisquam sequi maiores qui quidem dolores atque perspiciatis doloremque quae
+					quis laudantium corporis mollitia id saepe repudiandae accusamus explicabo
+					voluptatum deleniti. Enim doloremque in fugit velit officia consequuntur,
+					assumenda provident pariatur ab tempora laboriosam minus non commodi veritatis?
+					Labore perferendis fuga tempore molestiae vero excepturi, mollitia illum velit
+					aliquam eius iure? Sunt, veritatis similique. Enim numquam assumenda rerum
+					distinctio!
+				</h1>
+			</Canvas>
 		</>
 	);
 }
---
+++ src/components/Structures/Canvas.jsx
@@ -0,0 +1,25 @@
+import logo from "/logo.jpg";
+import PageHeader from "../Headers/PageHeader";
+
+const Canvas = ({ children, title }) => {
+	return (
+		<div className="relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-50 py-6 sm:py-1">
+			<PageHeader />
+			<img
+				src={logo}
+				alt=""
+				className="absolute top-1/2 left-1/2 max-w-none -translate-x-1/2 -translate-y-1/2 z-0"
+				width="1308"
+			/>
+
+			<div className="absolute inset-0 bg-center"></div>
+			<div className="max-h-[600px] overflow-y-auto">
+				<div className="relative bg-white px-6 pt-10 pb-8 shadow-l sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10">
+					{children}
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Canvas;
