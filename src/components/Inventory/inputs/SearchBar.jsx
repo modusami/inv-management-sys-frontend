75aabc79e42cd74cba780f5e7380a3519c14bdd2
modusami <128193218+modusami@users.noreply.github.com>
2024-03-17 03:13:57 -0400

    Update SearchBar.jsx

---
+++ src/components/Inventory/inputs/SearchBar.jsx
@@ -0,0 +1,26 @@
+const SearchBar = ({ onChange }) => {
+	return (
+		<div className="flex items-center border border-gray-300 rounded-lg p-2">
+			<svg
+				className="w-5 h-5 text-gray-400 mr-2"
+				xmlns="http://www.w3.org/2000/svg"
+				viewBox="0 0 20 20"
+				fill="currentColor"
+			>
+				<path
+					fillRule="evenodd"
+					d="M13.293 14.707a1 1 0 0 1-1.414 1.414l-2.5-2.5a5 5 0 1 1 1.414-1.414l2.5 2.5zM10 12a2 2 0 1 0 0-4 2 2 0 0 0 0 4z"
+					clipRule="evenodd"
+				/>
+			</svg>
+			<input
+				type="text"
+				placeholder="Search..."
+				className="outline-none bg-transparent"
+				onChange={onChange}
+			/>
+		</div>
+	);
+};
+
+export default SearchBar;
