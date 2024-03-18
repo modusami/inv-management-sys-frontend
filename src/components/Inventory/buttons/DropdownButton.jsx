0ab8787a240e1141472d8f1a8039a84bf7528dfb
modusami <128193218+modusami@users.noreply.github.com>
2024-03-17 23:34:57 -0400

    Create DropdownButton

---
+++ src/components/Inventory/buttons/DropdownButton.jsx
@@ -0,0 +1,20 @@
+const DropdownButton = ({ id, name, options }) => {
+	return (
+		<div className="flex flex-1 justify-center">
+			<select name={name} id={id} className="w-full">
+				{options.map((value, key) => {
+					return (
+						<>
+							<option className="text-center" key={key} value={value.toLowerCase()}>
+								Search By: {value}
+							</option>
+							;
+						</>
+					);
+				})}
+			</select>
+		</div>
+	);
+};
+
+export default DropdownButton;
