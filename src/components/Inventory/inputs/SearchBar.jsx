cdc32c598ef4e94fcac496dcf56894fa31e28e14
modusami <128193218+modusami@users.noreply.github.com>
2024-03-17 23:36:58 -0400

    Update Searchbar Styling

--- src/components/Inventory/inputs/SearchBar.jsx
+++ src/components/Inventory/inputs/SearchBar.jsx
@@ -1,6 +1,6 @@
 const SearchBar = ({ onChange }) => {
 	return (
-		<div className="flex items-center border border-gray-300 rounded-lg p-2">
+		<div className="flex flex-1 items-center border border-gray-300 rounded-lg p-2">
 			<svg
 				className="w-5 h-5 text-gray-400 mr-2"
 				xmlns="http://www.w3.org/2000/svg"
