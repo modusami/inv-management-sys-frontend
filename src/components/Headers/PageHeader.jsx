4b373ad6f3ddcab846cf9602b9e0023775d8960e
modusami <128193218+modusami@users.noreply.github.com>
2024-03-18 23:44:05 -0400

    Make header smaller

--- src/components/Headers/PageHeader.jsx
+++ src/components/Headers/PageHeader.jsx
@@ -1,7 +1,7 @@
 const PageHeader = () => {
 	return (
 		<div className="w-full flex items-center justify-center">
-			<p className="text-transparent bg-gradient-to-r from-pink-500 lowercase to-yellow-500 bg-clip-text font-semibold text-sm sm:text-sm md:text-l lg:text-xl xl:text-xl">
+			<p className="text-transparent bg-gradient-to-r from-pink-500 lowercase to-yellow-500 bg-clip-text font-semibold text-sm sm:text-sm md:text-l lg:text-md xl:text-md">
 				Inventory Management System By:{" "}
 				<span className="text-black lowercase">Odusami</span>
 			</p>
